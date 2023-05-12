8c74110b7768be394503ca5993b096ddfa1d9e4d
krutoo <dima.krutoo@yandex.ru>
2023-05-12 09:43:09 +0500

    draft test fixes

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,11 @@
 import { configureFetch, applyMiddleware } from '../src';
-import { baseURL, validateStatus } from '../src/middleware';
+import { validateStatus } from '../src/middleware';
 
 const myFetch = configureFetch(
   fetch,
-  applyMiddleware(
-    baseURL('https://jsonplaceholder.typicode.com/'),
-    validateStatus(status => status >= 200 && status < 300),
-  ),
+  applyMiddleware(validateStatus(status => status >= 200 && status < 300)),
 );
 
-myFetch('posts/1')
+myFetch('/posts/1')
   .then(res => res.json())
   .then(console.log);
