eee8a90b98c186033c4f93ca41bd05a0eaca69c4
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2023-10-25 12:14:19 +0500

    pkg
    - enhance tests

--- test/test.cjs
+++ test/test.cjs
@@ -1,14 +1,29 @@
-const { configureFetch, applyMiddleware } = require('@krutoo/fetch-tools');
-const { validateStatus } = require('@krutoo/fetch-tools/middleware');
+const fs = require('fs/promises');
+const path = require('node:path');
+const assert = require('node:assert');
 
-console.assert(typeof configureFetch === 'function');
-console.assert(typeof applyMiddleware === 'function');
+async function testExports() {
+  const pkg = JSON.parse(await fs.readFile('../package.json', 'utf-8'));
 
-const myFetch = configureFetch(
-  fetch,
-  applyMiddleware(validateStatus(status => status >= 200 && status < 300)),
-);
+  assert.equal(pkg.name, '@krutoo/fetch-tools');
 
-myFetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(res => res.json())
-  .then(console.log);
+  for (const pathname of Object.keys(pkg.exports)) {
+    const specifier = path.join(pkg.name, pathname);
+
+    assert.doesNotThrow(() => require(specifier));
+  }
+}
+
+async function testBaseFunctionality() {
+  const { configureFetch, applyMiddleware } = require('@krutoo/fetch-tools');
+  const { validateStatus } = require('@krutoo/fetch-tools/middleware');
+
+  assert.equal(typeof configureFetch, 'function');
+  assert.equal(typeof applyMiddleware, 'function');
+
+  const myFetch = configureFetch(fetch, applyMiddleware(validateStatus(status => status === 200)));
+
+  assert.equal(typeof myFetch, 'function');
+}
+
+Promise.resolve().then(testExports).then(testBaseFunctionality);
--- test/test.mjs
+++ test/test.mjs
@@ -1,14 +1,29 @@
-import { configureFetch, applyMiddleware } from '@krutoo/fetch-tools';
-import { validateStatus } from '@krutoo/fetch-tools/middleware';
+import fs from 'fs/promises';
+import path from 'node:path';
+import assert from 'node:assert';
 
-console.assert(typeof configureFetch === 'function');
-console.assert(typeof applyMiddleware === 'function');
+async function testExports() {
+  const pkg = JSON.parse(await fs.readFile('../package.json', 'utf-8'));
 
-const myFetch = configureFetch(
-  fetch,
-  applyMiddleware(validateStatus(status => status >= 200 && status < 300)),
-);
+  assert.equal(pkg.name, '@krutoo/fetch-tools');
 
-myFetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(res => res.json())
-  .then(console.log);
+  for (const pathname of Object.keys(pkg.exports)) {
+    const specifier = path.join(pkg.name, pathname);
+
+    await assert.doesNotReject(() => import(specifier));
+  }
+}
+
+async function testBaseFunctionality() {
+  const { configureFetch, applyMiddleware } = await import('@krutoo/fetch-tools');
+  const { validateStatus } = await import('@krutoo/fetch-tools/middleware');
+
+  assert.equal(typeof configureFetch, 'function');
+  assert.equal(typeof applyMiddleware, 'function');
+
+  const myFetch = configureFetch(fetch, applyMiddleware(validateStatus(status => status === 200)));
+
+  assert.equal(typeof myFetch, 'function');
+}
+
+Promise.resolve().then(testExports).then(testBaseFunctionality);
