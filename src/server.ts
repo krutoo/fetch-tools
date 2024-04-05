86d2c9d781be51d66c56d2352768729e2fb242da
krutoo <dima.krutoo@yandex.ru>
2024-04-05 19:05:30 +0500

    router-builder
    - `server`: `router.builder()` method added
    - readme updated

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import type { Handler } from './types.ts';
 
 interface Route {
-  is(url: URL, request: Request): boolean;
+  matches(url: URL, request: Request): boolean;
   handler: Handler;
 }
 
@@ -12,7 +12,7 @@ export function router(...routes: Route[]): Handler {
     const url = new URL(request.url);
 
     for (const route of routes) {
-      if (route.is(url, request)) {
+      if (route.matches(url, request)) {
         return route.handler(request);
       }
     }
@@ -21,16 +21,18 @@ export function router(...routes: Route[]): Handler {
   };
 }
 
+router.builder = builder;
+
 export function route(pattern: RoutePattern, handler: Handler): Route {
   if (typeof pattern === 'function') {
     return {
-      is: pattern,
+      matches: pattern,
       handler,
     };
   }
 
   return {
-    is: url => url.pathname === pattern,
+    matches: url => url.pathname === pattern,
     handler,
   };
 }
@@ -51,14 +53,93 @@ function createRouteFactoryForMethod(method: string) {
   return (pattern: RoutePattern, handler: Handler): Route => {
     if (typeof pattern === 'function') {
       return {
-        is: (url, req) => isSuitableMethod(req) && pattern(url, req),
+        matches: (url, req) => isSuitableMethod(req) && pattern(url, req),
         handler,
       };
     }
 
     return {
-      is: (url, request) => isSuitableMethod(request) && url.pathname === pattern,
+      matches: (url, request) => isSuitableMethod(request) && url.pathname === pattern,
       handler,
     };
   };
+}
+
+interface HandlerBuilder {
+  all(pattern: RoutePattern, handler: Handler): this;
+  get(pattern: RoutePattern, handler: Handler): this;
+  post(pattern: RoutePattern, handler: Handler): this;
+  put(pattern: RoutePattern, handler: Handler): this;
+  delete(pattern: RoutePattern, handler: Handler): this;
+  head(pattern: RoutePattern, handler: Handler): this;
+  options(pattern: RoutePattern, handler: Handler): this;
+  connect(pattern: RoutePattern, handler: Handler): this;
+  patch(pattern: RoutePattern, handler: Handler): this;
+  build(): Handler;
+}
+
+function builder(): HandlerBuilder {
+  const routeList: Array<Route> = [];
+
+  const builder: HandlerBuilder = {
+    all(pattern, handler) {
+      routeList.push(route.all(pattern, handler));
+
+      return builder;
+    },
+
+    get(pattern, handler) {
+      routeList.push(route.get(pattern, handler));
+
+      return builder;
+    },
+
+    post(pattern, handler) {
+      routeList.push(route.post(pattern, handler));
+
+      return builder;
+    },
+
+    put(pattern, handler) {
+      routeList.push(route.put(pattern, handler));
+
+      return builder;
+    },
+
+    delete(pattern, handler) {
+      routeList.push(route.delete(pattern, handler));
+
+      return builder;
+    },
+
+    head(pattern, handler) {
+      routeList.push(route.head(pattern, handler));
+
+      return builder;
+    },
+
+    options(pattern, handler) {
+      routeList.push(route.options(pattern, handler));
+
+      return builder;
+    },
+
+    connect(pattern, handler) {
+      routeList.push(route.connect(pattern, handler));
+
+      return builder;
+    },
+
+    patch(pattern, handler) {
+      routeList.push(route.patch(pattern, handler));
+
+      return builder;
+    },
+
+    build() {
+      return router(...routeList);
+    },
+  };
+
+  return builder;
 }
